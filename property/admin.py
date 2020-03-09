d7b5881d7eab5ef0caedbf060b1ba50522e3008d
Vladimir <smirnovv91@gmail.com>
2020-03-09 16:58:19 +0300

    Normalize Flat.phonenumbers into Flat.owner_phone_pure

--- property/admin.py
+++ property/admin.py
@@ -6,7 +6,7 @@ from .models import Flat, Complaint
 class FlatAdmin(admin.ModelAdmin):
     readonly_fields = ('created_at',)
     search_fields = ('town','address','owner',)
-    list_display = ('address', 'price', 'new_building', 'construction_year', 'town')
+    list_display = ('address', 'price', 'new_building', 'construction_year', 'town', 'owner_phone_pure')
     list_editable = ('new_building',)
     list_filter = ('new_building',)
     raw_id_fields = ('likes',)
--- property/models.py
+++ property/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+from phonenumber_field.modelfields import PhoneNumberField
 
 class Flat(models.Model):
     owner = models.CharField("ФИО владельца", max_length=200)
@@ -25,6 +26,7 @@ class Flat(models.Model):
 
     new_building = models.NullBooleanField()
     likes = models.ManyToManyField(User)
+    owner_phone_pure = PhoneNumberField(null=True, blank=True, verbose_name='Нормализованный номер владельца')
 
     def __str__(self):
         return f"{self.town}, {self.address} ({self.price}р.)"
